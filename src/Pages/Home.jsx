f7bd8e1783a8c31c3fa197ceb40e707517c8e7d3
Abhishek Kokate <Abhishekkokate8@gmail.com>
2024-03-25 00:14:45 +0530

    Calender Date imporvements and made dynamic from state data

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,24 @@
 import Calendar from "react-calendar";
 import "../assets/styles/calender.css";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import checkCircle from "../assets/icons/circle-check-solid.svg";
 
 const Home = () => {
   // States and Variables
   const disabledDays = [0, 6];
+  const [attenenceData, setAttenenceData] = useState({
+    "2024-2-19": true,
+    "2024-2-20": false,
+    "2024-2-21": null,
+  });
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [isTodaySelected, setIsTodaySelected] = useState(false);
   const [attendedToday, setAttendedToday] = useState(false);
 
   // Functions
   const convertToDate = (date) => {
     try {
-      date = date.toISOString(); //'2024-03-24T13:20:31.715Z'
-      date = date.split("T")[0]; //'2024-03-24'
+      date = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
     } catch (err) {
       console.error(err);
     }
@@ -22,6 +27,12 @@ const Home = () => {
 
   // Etc
   const calendar = useRef();
+
+  useEffect(() => {
+    setIsTodaySelected(
+      convertToDate(selectedDate) === convertToDate(new Date())
+    );
+  }, [selectedDate]);
 
   return (
     <div className="container-main calender-container">
@@ -32,26 +43,23 @@ const Home = () => {
           value={selectedDate}
           defaultActiveStartDate={selectedDate}
           tileDisabled={
-            ({ date }) => disabledDays.includes(date.getDay()) //Disabled Sunday(0), Saturday(6)
+            ({ date }) => disabledDays.includes(date.getDay()) //Disable Sunday(0), Saturday(6)
           }
           tileClassName={({ date, view }) => {
             if (view !== "month") {
               return "";
             }
             let classesToReturn = "calender-selector-date";
-            if (disabledDays.includes(date.getDay())) {
-              return classesToReturn;
+            if (convertToDate(date) === convertToDate(selectedDate)) {
+              return (classesToReturn += " active-date");
             }
 
-            if (convertToDate(date) === convertToDate(selectedDate)) {
-              classesToReturn += " active-date";
-            } else {
-              if (Math.random() * 10 > 5) {
-                classesToReturn += " success";
-              } else {
-                classesToReturn += (Math.random() * 10 > 5 && " failed") || "";
-              }
+            if (attenenceData[convertToDate(date)] === true) {
+              classesToReturn += " success";
+            } else if (attenenceData[convertToDate(date)] === false) {
+              classesToReturn += " failed";
             }
+
             return classesToReturn;
           }}
         />
@@ -81,17 +89,36 @@ const Home = () => {
         </div>
       </div>
       <div className="calender-mark-today">
-        {attendedToday ? (
+        {attenenceData?.[convertToDate(selectedDate)] === true && (
           <>
-            <h4>Attendance for today is already marked </h4>
+            <h4>
+              Attendance for {convertToDate(selectedDate)} is already marked{" "}
+            </h4>
             <img className="checked-green" src={checkCircle} alt="Checked" />
           </>
-        ) : (
+        )}
+        {attenenceData?.[convertToDate(selectedDate)] === false && (
+          <>
+            <h4>Attendance for {convertToDate(selectedDate)} is Not marked </h4>
+          </>
+        )}
+        {!isTodaySelected &&
+          !attenenceData?.[convertToDate(selectedDate)] &&
+          attenenceData?.[convertToDate(selectedDate)] !== false && (
+            <>
+              <h4>Attendance for {convertToDate(selectedDate)} is Pending </h4>
+            </>
+          )}
+
+        {isTodaySelected && !attendedToday && (
           <>
             <h4>Mark Attendance for Today: </h4>
             <button
               className="btn-primary"
-              onClick={() => setAttendedToday(true)}
+              onClick={() => {
+                attenenceData[new Date()] = true;
+                setAttendedToday(true);
+              }}
             >
               Mark
             </button>
