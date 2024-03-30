1d8481cf4ddbe85af0f8a780e6adc848dec18ff3
Abhishek Kokate <Abhishekkokate8@gmail.com>
2024-03-30 20:22:29 +0530

    Frontend moved to separate folder

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { LoadingProvider } from "./contexts/LoadingContext";
+import { LoadingProvider } from "./contexts/LoadingContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
